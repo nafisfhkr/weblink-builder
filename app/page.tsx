734fefab77e310920d1bb4bae85a3aac8a8755c0
Nafis Fakhrudin <nafisfakhru@gmail.com>
2026-07-08 14:52:23 +0700

    Pull hero content closer to navbar

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default async function LandingPage() {
       </nav>
 
       {/* Hero Section */}
-      <section className="relative h-[100dvh] min-h-[650px] pt-24 lg:pt-16 pb-8 flex flex-col justify-center overflow-hidden px-6">
+      <section className="relative h-[100dvh] min-h-[650px] pt-[120px] lg:pt-[140px] pb-8 flex flex-col justify-start overflow-hidden px-6">
         
         {/* Abstract Background Elements */}
         <div className="absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-teal-900/20 blur-[120px] rounded-full pointer-events-none" />
